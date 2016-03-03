6e1070d86577e4843165f390da89feca0270b784
Dylan Walseth <dylanw@mtolympus.partners.directs.com>
2016-03-03 12:32:27 -0600

    Test2

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var gpio = require("pi-gpio");
 
 gpio.open(11, "output", function(err) {
     gpio.write(11, 1, function() {
+        setTimeout(function() {
+        }, 5000);
         gpio.close(11);
     });
 });
-
-setTimeout(function() {
-}, 5000);
